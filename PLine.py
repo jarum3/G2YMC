9d711511c3877af95fe998b4a36a36ec9fafaf9b
jSumnerd <83377672+jSumnerd@users.noreply.github.com>
2023-10-26 18:49:25 -0400

    Compiler and PLine
    Create shell of compiler and created PLine class. Its the line class we discussed but the name is changed to avoid confusion with the line keyword in python.

---
+++ PLine.py
@@ -0,0 +1,21 @@
+#######################################################################
+#   CSC 365 Project Group 2
+#   Jacob Duncan, Jakob Robinson, Terry Townsend, Brad Kivett
+#
+#   PLine class,  called it PLine so it wouldn't be confused with
+#   the line keyword
+#
+#######################################################################
+
+
+class PLine:
+    def __init__(self, text, type):
+        self.text = text
+        self.type = type
+        self.YMC_string = None # this will be updated by our switch statement
+        self.assembly_string = None
+
+        if self.text.startswith('   '):  # Modify this condition based on the type of indentation you expect
+            # define parent here
+            # self.parent = [PARENT LINE]
+            print("Line starts with indentation:")
