1e4087e0b6e1b46b7e99e9bb50e93147f929c224
jarum3 <117324735+jarum3@users.noreply.github.com>
2023-10-28 19:36:40 -0400

    bugfix

--- src/ymc/helpers/setFlags.py
+++ src/ymc/helpers/setFlags.py
@@ -1,4 +1,4 @@
-from helpers.binaryConversion import unsignedIntToBinary as bc
+from helpers.binaryConversion import unsignedIntToBinary
 
 
 def setFlags(result: int,
@@ -11,7 +11,7 @@ def setFlags(result: int,
         "ZF": False
     }
     if (result == 0): flags["ZF"] = True
-    if (bc.unsignedIntToBinary(result)[0] == "1"): flags["SF"] = True
+    if (unsignedIntToBinary(result)[0] == "1"): flags["SF"] = True
     if (result > 127 or result < -128): flags["OF"] = True
     if (result > 255 and add): flags["CF"] = True
     if (subCF): flags["CF"] = True
