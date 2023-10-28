042e1b0f13bfe4ea801b1d5979f97fc5ef366004
jarum3 <117324735+jarum3@users.noreply.github.com>
2023-10-28 10:13:57 -0400

    Finishing out and adding mov functions

--- src/ymc/instructions/asmFunctions.py
+++ src/ymc/instructions/asmFunctions.py
@@ -6,3 +6,35 @@ import ymc.helpers.binaryConversion as bc
 def outs(register: str) -> None:
     # print the signed representation of the CPU register pointed to by the register byte
     print(bc.signedBinaryToInt(cpu.registers[rl.fourBitToRegister(register)]))
+
+
+def outu(register: str) -> None:
+    # print the unsigned representation of the CPU register pointed to by the register byte
+    print(bc.unsignedBinaryToInt(
+        cpu.registers[rl.fourBitToRegister(register)]))
+
+
+def outnl() -> None:
+    print("\n")
+
+
+def movrr(registers: str) -> None:
+    args: list[str] = rl.eightBitToRegisters(registers)
+    cpu.registers[args[0]] = cpu.registers[args[1]]
+
+
+def movmrm(register: str, address: str) -> None:
+    reg = rl.fourBitToRegister(register)
+    addr_int: int = bc.BinaryToAddr(address)
+    cpu.registers[reg] = cpu.memory[addr_int]
+
+
+def movrl(register: str, literal: str) -> None:
+    reg = rl.fourBitToRegister(register)
+    cpu.registers[reg] = literal[-8:]
+
+
+def movmr(address: str, register: str) -> None:
+    reg = rl.fourBitToRegister(register)
+    addr_int = bc.BinaryToAddr(address)
+    cpu.memory[addr_int] = cpu.registers[reg]
