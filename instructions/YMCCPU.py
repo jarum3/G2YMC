8c6ddaa3f375ed1b5089df186dde57176a2c0fad
jarum3 <117324735+jarum3@users.noreply.github.com>
2023-10-28 09:07:03 -0400

    Capitalizing register names

--- instructions/YMCCPU.py
+++ instructions/YMCCPU.py
@@ -1,4 +1,4 @@
-registers: dict[str, int] = {"eax": 0, "ebx": 0, "ecx": 0, "edx": 0}
+registers: dict[str, int] = {"EAX": 0, "EBX": 0, "ECX": 0, "EDX": 0}
 flags: dict[str, bool] = {"OF": False, "SF": False, "CF": False, "ZF": False}
 memory: list[str] = ["00000000"] * 1024
 instructionPointer: int = 0
