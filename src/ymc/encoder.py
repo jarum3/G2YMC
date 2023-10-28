239d1cc875f1442b635e6d6f7e9aafa4c61869e3
jarum3 <117324735+jarum3@users.noreply.github.com>
2023-10-28 10:55:12 -0400

    Organizing as script

--- src/ymc/encoder.py
+++ src/ymc/encoder.py
@@ -1,6 +1,15 @@
-# Should use files asm.pkl to convert from assembly code to binary
-# asm.pkl should contain object files for all instructions, with their assigned hex code and length being used to create binary data
 import pickle
 from instructions.Instruction import Instruction
-with open("instructions/asm.pkl", "rb") as file:
-    instructions: list[Instruction] = pickle.load(file)
+
+
+def main():
+    # Should use files asm.pkl to convert from assembly code to binary
+    # asm.pkl should contain object files for all instructions, with their assigned hex code and length being used to create binary data
+
+    with open("instructions/asm.pkl", "rb") as file:
+        instructions: list[Instruction] = pickle.load(file)
+    print(instructions)
+
+
+if __name__ == "__main__":
+    main()
