12c465e5006214e4c55d7ebf143047773243bffa
jarum3 <117324735+jarum3@users.noreply.github.com>
2023-10-28 14:06:52 -0400

    Adding flag to set flags from arithmetic

---
+++ src/ymc/helpers/setFlags.py
@@ -0,0 +1,18 @@
+from binaryConversion import unsignedIntToBinary as bc
+
+
+def setFlags(result: int,
+             add: bool = False,
+             subCF: bool = False) -> dict[str, bool]:
+    flags: dict[str, bool] = {
+        "OF": False,
+        "SF": False,
+        "CF": False,
+        "ZF": False
+    }
+    if (result == 0): flags["ZF"] = True
+    if (bc.unsignedIntToBinary(result)[0] == "1"): flags["SF"] = True
+    if (result > 127 or result < -128): flags["OF"] = True
+    if (result > 255 and add): flags["CF"] = True
+    if (subCF): flags["CF"] = True
+    return flags
