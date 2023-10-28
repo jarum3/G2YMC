18ffbca9c21ef2c75c6c8b2d16277c3869994263
jarum3 <117324735+jarum3@users.noreply.github.com>
2023-10-27 22:31:43 -0400

    Adding dict of widths for data types

--- instructions/YMCCPU.py
+++ instructions/YMCCPU.py
@@ -2,3 +2,9 @@ registers: dict[str, int] = {"eax": 0, "ebx": 0, "ecx": 0, "edx": 0}
 flags: dict[str, bool] = {"OF": False, "SF": False, "CF": False, "ZF": False}
 memory: list[str] = ["00000000"] * 1024
 instructionPointer: int = 0
+typeWidths: dict[str, int] = {
+    "literal": 1,
+    "register": 1,
+    "register-register": 1,
+    "memory": 2
+}
