128ca7315134e8ce565113bd2413cdfb88bc6a09
jarum3 <117324735+jarum3@users.noreply.github.com>
2023-10-27 21:46:46 -0400

    Adding processing for output end of pickle file

--- encoder.py
+++ encoder.py
@@ -1,2 +1,6 @@
 # Should use files asm.pkl to convert from assembly code to binary
 # asm.pkl should contain object files for all instructions, with their assigned hex code and length being used to create binary data
+import pickle
+from instructions.Instruction import Instruction
+with open("instructions/asm.pkl", "rb") as file:
+    instructions: list[Instruction] = pickle.load(file)
