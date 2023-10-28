061df79bd33da6c76cfea77531725e893a4b5d97
jarum3 <117324735+jarum3@users.noreply.github.com>
2023-10-28 15:16:39 -0400

    Marking route to move forward

--- src/ymc/simulator.py
+++ src/ymc/simulator.py
@@ -6,13 +6,17 @@ import YMCCPU as cpu
 def main():
     with open("instructions/asm.pkl", "rb") as file:
         instructions: dict[str, Instruction] = pickle.load(file)
+    # TODO: Edit this to first read the file into the start of memory as strings, then perform operations on that memory.
     with open("file.bin", "rb") as file:
-        while (byte := file.read(1)):
+        while (True):  # Halt instruction should call exit()
+            file.seek(cpu.instructionPointer)
+            byte = file.read(1)
             currHex = byte.hex()
             currInstr: Instruction = instructions[currHex]
             args: list[str] = []
             for i, arg in enumerate(currInstr.argTypes):
                 args[i] = f'{file.read(cpu.typeWidths[arg]):0>8b}'
+            cpu.instructionPointer += currInstr.width
             currInstr.function(*args)
 
 
