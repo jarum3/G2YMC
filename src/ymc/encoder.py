61886105e8f8f2c217f24c229cd051c99e2e07ac
jarum3 <117324735+jarum3@users.noreply.github.com>
2023-10-28 11:55:19 -0400

    Adding comments for encoding process

--- src/ymc/encoder.py
+++ src/ymc/encoder.py
@@ -9,6 +9,19 @@ def main():
     with open("instructions/asm.pkl", "rb") as file:
         instructions: list[Instruction] = pickle.load(file)
     print(instructions)
+    # Open input file
+
+    # Loop through lines of input file
+
+    # Convert line into instruction and arguments
+
+    # Read argument types from instruction
+
+    # Convert arguments into binary
+
+    # Store instruction with arguments in binary
+
+    # Move to next line
 
 
 if __name__ == "__main__":
