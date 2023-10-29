9e812b5282dfcb67d615e51313b6dfcdc1e0a9d7
jarum3 <117324735+jarum3@users.noreply.github.com>
2023-10-28 21:19:23 -0400

    Encoder bugfixes

--- src/ymc/encoder.py
+++ src/ymc/encoder.py
@@ -2,6 +2,7 @@ import pickle
 from instructions.Instruction import Instruction
 import helpers.binaryConversion as bc
 import helpers.registerLookup as rl
+import re
 
 
 def main():
@@ -11,41 +12,39 @@ def main():
     with open("instructions/instructionsByName.pkl", "rb") as file:
         instructions: dict[str, Instruction] = pickle.load(file)
     # Open input file
-    with open("file.asm", "r") as file:
+    with open("file.ymc", "r") as file:
         for line in file:
             # Loop through lines of input file
             if (len(line)) > 0:
-                pieces = line.split(" ")
+                lineTrimmed = re.sub(r'[^A-Za-z0-9 ]+', '', line)
+                pieces = lineTrimmed.split(" ")
                 currInstr = pieces[0]
                 args = pieces[1:]
                 # Convert line into instruction and arguments
                 binary: list[str] = []
-                binary[0] = bc.hexToBinary(instructions[currInstr].hexCode)
+                binary.append(bc.hexToBinary(instructions[currInstr].hexCode))
                 argTypes = instructions[currInstr].argTypes
                 if args and argTypes:
                     # Read argument types from instruction
-                    byteCounter = 1
                     i = 0
                     while i < len(args):
                         if argTypes[i] == 'memory':
                             address = bc.addrToBinary(int(args[i]))
-                            binary[byteCounter] = address[8:16]
-                            binary[byteCounter + 1] = address[0:8]
-                            byteCounter += 1
+                            binary.append(address[8:16])
+                            binary.append(address[0:8])
                         elif argTypes[i] == 'register':
-                            binary[byteCounter] = rl.registerToFourBit(args[i])
+                            binary.append(rl.registerToFourBit(args[i]))
                         elif argTypes[i] == 'register-register':
-                            binary[byteCounter] = rl.registersToEightBit(
-                                args[i], args[i + 1])
+                            binary.append(
+                                rl.registersToEightBit(args[i], args[i + 1]))
                             i += 1
                         elif argTypes[i] == 'literal':
                             if (args[i][0] == "-"):
-                                binary[byteCounter] = bc.signedIntToBinary(
-                                    int(args[i]))
+                                binary.append(
+                                    bc.signedIntToBinary(int(args[i])))
                             else:
-                                binary[byteCounter] = bc.unsignedIntToBinary(
-                                    int(args[i]))
-                        byteCounter += 1
+                                binary.append(
+                                    bc.unsignedIntToBinary(int(args[i])))
                         i += 1
                 for byte in binary:
                     binaryString += byte
