d9ab48e3eef6fce665ac65fed1bcdaf84cb03fd7
jSumnerd <83377672+jSumnerd@users.noreply.github.com>
2023-10-30 12:52:44 -0400

    Compiler and Pline changes
    What we did during meeting.

--- src/ymc/PLine.py
+++ src/ymc/PLine.py
@@ -9,16 +9,27 @@
 
 
 class PLine:
-
-    def __init__(self, text: str, type: int):
+    def __init__(self, text):
         self.text = text
-        self.type = type
-        self.YMC_string: str | None = None  # this will be updated by our switch statement
-        self.assembly_string: str | None = None
+        self.YMC_string = None # this will be updated by our switch statement
+        self.assembly_string = None
+        self.isParent = False
 
-        if self.text.startswith(
-                '   '
-        ):  # Modify this condition based on the type of indentation you expect
-            # define parent here
-            # self.parent = [PARENT LINE]
-            print("Line starts with indentation:")
+        if self.text.startswith("signed" or "unsigned"): # delclaration
+            self.type = 1
+
+        if self.text.startswith("if" or "else" or "while"): # relational
+            self.type = 3
+            self.isParent = True
+
+        if self.text.includes("=" or"+" or "-" or "*" or "/"): # arithmetic
+            self.type = 2
+
+        if self.text.startswith("print"): # print
+            self.type = 4
+
+    def setYMC(ymc): # this is how we will store the YMC string
+        print("error handling")
+
+    def setParent(self, parent):
+        self.parent = parent
--- src/ymc/compiler.py
+++ src/ymc/compiler.py
@@ -10,14 +10,10 @@ from PLine import PLine
 
 program_counter = 0
 
-# EDX    ECX    EBX    EAX
-registers = [
-    False, False, False, False
-]  # python is weird and won't let you define a boolean array with 0 and 1 so we have to use true and false here
-# ZF     SF     OF     CF
-flags = [
-    False, False, False, False
-]  # maybe I'm wrong and one of you guys can figure out how to be able to use 0 and 1
+            # EDX    ECX    EBX    EAX
+registers =  [0, 0, 0, 0] # python is weird and won't let you define a boolean array with 0 and 1 so we have to use true and false here
+        # ZF     SF     OF     CF
+flags =  [False, False, False, False]     # maybe I'm wrong and one of you guys can figure out how to be able to use 0 and 1
 
 # This is how chat GPT told me to make a list with a specified size. Not sure if there is a better way or not
 size = 1000  # 1 kb is 1000 bytes
@@ -25,50 +21,47 @@ default_value = 0  # Define the default value.
 # Memory array here. Not sure what type to make it
 memory = [default_value] * size
 
-
-# This stuff is for the switch statement
-def declaration():  # start by checking if signed or unsigned
-    print(
-        "This is case 1"
-    )  # I left these print statments here to eliminate the error caused by having no code in the function
-
-
-def arithmetic():  # start by checking if 2 arguments or 3 arguments
-    print("This is case 2")
-
-
-def relational(
-):  # if/else and while statements, start by checking what each line is
-    print("This is case 3")
-
-
-def printD():
-    print("This is case 4")
-
-
-def default_case():
-    print("This is the default case")
-
-
-switch_dict = {1: declaration, 2: arithmetic, 3: relational, 4: printD}
-
-
 def main():
     # Open the file in read mode
-    file_path = "path_to_your_file.txt"  # Replace with the actual path to txt file
+    file_path = "path_to_your_file.txt"  # Replace with the actual path to txt file 
     try:
-        with open(file_path, 'r') as file:  # the r here means read only mode
+        with open(file_path, 'r') as file: # the r here means read only mode
             # Iterate over each line in the file
-            for line in file:  # Process each line here
+            for line in file: # Process each line here
                 # Create a line object. The constructor for this class will check if it is an indented line and automatically set its parent line if so.
                 # It will also set a flag telling us what category it belongs to (print, declaration, arithmetic operation, while or if)
-                pline_instance: PLine = PLine(
-                    line, 1
-                )  # the first parameter is the text of the line, the second is the type SEE SWITCH DICTIONARY
+                pline_instance = PLine(line) # the first parameter is the text of the line, the second is the type SEE SWITCH DICTIONARY
+                
+                if pline_instance.text.startswith('   '):  # Modify this condition based on the type of indentation you expect
+                    # pline_instance.setParent([ITEM IN LINE ARRAY])
+                    print("Line starts with indentation:")
 
                 # this line will execute the corresponding function based on the line type
                 switch_dict.get(pline_instance.type, default_case)()
 
+                # This stuff is for the switch statement
+                def declaration(): # start by checking if signed or unsigned
+                    print("This is case 1") # I left these print statments here to eliminate the error caused by having no code in the function
+
+                def arithmetic(): # start by checking if 2 arguments or 3 arguments
+                    print("This is case 2")
+
+                def relational(): # if/else and while statements, start by checking what each line is
+                    print("This is case 3")
+
+                def printD(): 
+                    print("This is case 4")
+
+                def default_case():
+                    print("This is the default case")
+
+                switch_dict = {
+                    1: declaration,
+                    2: arithmetic,
+                    3: relational,
+                    4: printD
+                }
+    
     except FileNotFoundError:
         print("File not found. Please check the file path.")
     except Exception as e:
