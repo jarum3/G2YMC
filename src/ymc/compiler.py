6b4bccff56492fc626e2c4a9032b4a11b871b5ca
jarum3 <117324735+jarum3@users.noreply.github.com>
2023-10-30 13:03:25 -0400

    Moving declarations above main body to avoid error

--- src/ymc/compiler.py
+++ src/ymc/compiler.py
@@ -21,6 +21,27 @@ default_value = 0  # Define the default value.
 # Memory array here. Not sure what type to make it
 memory = [default_value] * size
 
+### All of these have to be initialized before the switch_dict.get statement
+
+# This stuff is for the switch statement
+def declaration(): # start by checking if signed or unsigned
+    print("This is case 1") # I left these print statments here to eliminate the error caused by having no code in the function
+def arithmetic(): # start by checking if 2 arguments or 3 arguments
+    print("This is case 2")
+def relational(): # if/else and while statements, start by checking what each line is
+    print("This is case 3")
+def printD(): 
+    print("This is case 4")
+def default_case():
+    print("This is the default case")    
+
+switch_dict = {
+    1: declaration,
+    2: arithmetic,
+    3: relational,
+    4: printD
+}
+
 def main():
     # Open the file in read mode
     file_path = "path_to_your_file.txt"  # Replace with the actual path to txt file 
@@ -35,33 +56,10 @@ def main():
                 if pline_instance.text.startswith('   '):  # Modify this condition based on the type of indentation you expect
                     # pline_instance.setParent([ITEM IN LINE ARRAY])
                     print("Line starts with indentation:")
-
+                
                 # this line will execute the corresponding function based on the line type
                 switch_dict.get(pline_instance.type, default_case)()
 
-                # This stuff is for the switch statement
-                def declaration(): # start by checking if signed or unsigned
-                    print("This is case 1") # I left these print statments here to eliminate the error caused by having no code in the function
-
-                def arithmetic(): # start by checking if 2 arguments or 3 arguments
-                    print("This is case 2")
-
-                def relational(): # if/else and while statements, start by checking what each line is
-                    print("This is case 3")
-
-                def printD(): 
-                    print("This is case 4")
-
-                def default_case():
-                    print("This is the default case")
-
-                switch_dict = {
-                    1: declaration,
-                    2: arithmetic,
-                    3: relational,
-                    4: printD
-                }
-    
     except FileNotFoundError:
         print("File not found. Please check the file path.")
     except Exception as e:
