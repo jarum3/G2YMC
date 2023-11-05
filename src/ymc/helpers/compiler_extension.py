eb6b0db168498e2c3ddd6e16a1af5cd7d9941fc6
bradkivett <bradley.kivett98@gmail.com>
2023-11-04 21:17:35 -0400

    created dictionaries
    Dictionary for variables being set to true or false and dictionary of their values

--- src/ymc/helpers/compiler_extension.py
+++ src/ymc/helpers/compiler_extension.py
@@ -9,6 +9,9 @@ registers: dict[str, int] = {"EDX": 0, "ECX": 0, "EBX": 0, "EAX": 0}
 
 flags: dict[str, bool] = {"OF": False, "SF": False, "CF": False, "ZF": False}  # I'm getting a runtime error here. It's goes away by adding ' ' around dict[str, bool] --Brad
 
+variables_set: dict[str, bool] = {"a": False, "b": False, "c": False, "x": False, "y": False, "z": False}
+variable_value: dict[str, int] = {"a": 0, "b": 0, "c": 0, "x": 0, "y": 0, "z": 0}
+
 size = 1024  # 1 kb is 1000 bytes
 default_value = 0  # Define the default value.
 memory = [default_value] * size
@@ -35,21 +38,17 @@ def set_parent(pline_instance, pline_list, i):
 
 # This stuff is for the switch statement
 
-##########################
-# Variable Declaration
-# Brad K
-##########################
 def declaration(pline_instance): # start by checking if signed or unsigned
     line_text = pline_instance.text # grab text from line
     variables = line_text.split()   # split variables in line into list. Im not sure if there's going to be HLC with less than 3 variables
     del variables[0]                # delete signed/unsigned word from variables list
 
-    if line_text.startswith("signed"):  # start by checking if signed or unsigned
-        flags["SF"] = True                        # set flag to True if signed
+    if line_text.startswith("unsigned"):  # start by checking if signed or unsigned
+        flags["SF"] = False                      # set flag to False if unsigned
         for v in variables:               # go through each variable and assign to location in storage
             print(v)
-    elif pline_instance.text.startswith("unsigned"):
-        flags["SF"] = False # set flag to False if unsigned
+    elif pline_instance.text.startswith("signed"):
+        flags["SF"] = True # set flag to True if signed
         for v in variables:               # go through each variable and assign to location in storage
             print(v)                      # Currently just printing
 
