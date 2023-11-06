834b9d3f966cfad4e366fe62058af0ad4b1d9dbe
jarum3 <117324735+jarum3@users.noreply.github.com>
2023-11-05 20:19:13 -0500

    Removing TODO for little-endian memory

--- src/ymc/simulator.py
+++ src/ymc/simulator.py
@@ -1,2 +1,1 @@
 # TODO: Edit this to first read the file into the start of memory as strings, then perform operations on that memory.
-# TODO: Add special case for Memory to process 2 bytes into one argument (little-endian)
