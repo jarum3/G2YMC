3c72bd17453a5bc48f47e84379db324424a513c0
jarum3 <117324735+jarum3@users.noreply.github.com>
2023-11-06 13:13:42 -0500

    Extracting encoding logic to importable function

--- src/ymc/encoder.py
+++ src/ymc/encoder.py
@@ -9,57 +9,66 @@ import helpers.binaryConversion as bc
 import helpers.registerLookup as rl
 import re
 
+def getBinaryFromLine(line: str, instructions: dict[str, Instruction]) -> str:
+    binaryString = ""
+    if (len(line)) > 0:
+        # Remove everything that isn't alphanumeric, negative, or a space (Also removes commas)
+        lineTrimmed = re.sub(r"[^-A-Za-z0-9 ]+", "", line)
+        # Split string on spaces (Instr at pieces[0], arguments at rest)
+        pieces = lineTrimmed.split(" ")
+        # Extracts current instruction
+        currInstr = pieces[0]
+        # Extracts arguments
+        args = pieces[1:]
+        # Convert line into instruction and arguments
+        binary: list[str] = []
+        # Add binary form of string to string
+        binary.append(bc.hexToBinary(instructions[currInstr].hexCode))
+        # Get array of argument types
+        argTypes = instructions[currInstr].argTypes
+        if args and argTypes:  # Just making sure we have arguments to process
+            # Read argument types from instruction
+            arg = 0
+            linePiece = 0
+            while arg < len(argTypes):  # Stop processing once we're done with arguments
+                match argTypes[arg]:
+                    case "memory": # Memory address
+                        address = bc.addrToBinary(int(args[linePiece]))
+                        binary.append(address)
+                    case "register": # One register in one byte
+                        binary.append(rl.registerToFourBit(args[linePiece]))
+                    case "register-register": # Both registers to one-byte (Two arguments in one match)
+                        binary.append(rl.registersToEightBit(args[linePiece], args[linePiece + 1]))
+                        linePiece += 1 # We're grabbing two arguments (this is why line piece and argument are counted separately)
+                    case "literal":
+                        if args[linePiece][0] == "-": # If number starts with negative, it definitely has to be signed
+                            binary.append(bc.signedIntToBinary(int(args[linePiece])))
+                        else: # Otherwise, interpreting as unsigned gives larger range
+                            binary.append(bc.unsignedIntToBinary(int(args[linePiece])))
+                # Increment both list pointers forward
+                arg += 1
+                linePiece += 1
+                # Append current instruction to binary string
+                for byte in binary:
+                    if byte:
+                        binaryString += byte
+    return binaryString
 
 def main():
     # Should use files asm.pkl to convert from assembly code to binary
     # asm.pkl should contain object files for all instructions, with their assigned hex code and length being used to create binary data
-    binaryString = ""
+    binaryString: str = ""
     with open("instructions/instructionsByName.pkl", "rb") as file:
         instructions: dict[str, Instruction] = pickle.load(file)
     # Open input file
     with open("file.ymc", "r") as file:
         for line in file:
-            # Loop through lines of input file
-            if (len(line)) > 0:
-                # Remove everything that isn't alphanumeric, negative, or a space (Also removes commas)
-                lineTrimmed = re.sub(r"[^-A-Za-z0-9 ]+", "", line)
-                # Split string on spaces (Instr at pieces[0], arguments at rest)
-                pieces = lineTrimmed.split(" ")
-                # Extracts current instruction
-                currInstr = pieces[0]
-                # Extracts arguments
-                args = pieces[1:]
-                # Convert line into instruction and arguments
-                binary: list[str] = []
-                # Add binary form of string to string
-                binary.append(bc.hexToBinary(instructions[currInstr].hexCode))
-                # Get array of argument types
-                argTypes = instructions[currInstr].argTypes
-                if args and argTypes:  # Just making sure we have arguments to process
-                    # Read argument types from instruction
-                    arg = 0
-                    linePiece = 0
-                    while arg < len(argTypes):  # Stop processing once we're done with arguments
-                        match argTypes[arg]:
-                            case "memory": # Memory address
-                                address = bc.addrToBinary(int(args[linePiece]))
-                                binary.append(address)
-                            case "register": # One register in one byte
-                                binary.append(rl.registerToFourBit(args[linePiece]))
-                            case "register-register": # Both registers to one-byte (Two arguments in one match)
-                                binary.append(rl.registersToEightBit(args[linePiece], args[linePiece + 1]))
-                                linePiece += 1 # We're grabbing two arguments (this is why line piece and argument are counted separately)
-                            case "literal":
-                                if args[linePiece][0] == "-": # If number starts with negative, it definitely has to be signed
-                                    binary.append(bc.signedIntToBinary(int(args[linePiece])))
-                                else: # Otherwise, interpreting as unsigned gives larger range
-                                    binary.append(bc.unsignedIntToBinary(int(args[linePiece])))
-                        # Increment both list pointers forward
-                        arg += 1
-                        linePiece += 1
-                # Append current instruction to binary string
-                for byte in binary:
-                    binaryString += byte
+            # Loop through lines of input files
+            binary = getBinaryFromLine(line, instructions)
+            print(binary)
+            if binary:
+                binaryString += binary
+    print(binaryString)
     # Write binary string
     with open("file.bin", "w") as file:
         file.write(binaryString)
