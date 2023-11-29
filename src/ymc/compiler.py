bebec70816f387d64554b71d2f8d3a382586384c
jSumnerd <83377672+jSumnerd@users.noreply.github.com>
2023-11-29 16:18:16 -0500

    Small change to compiler
    Just fixing some thing that were lost in the merge

--- src/ymc/compiler.py
+++ src/ymc/compiler.py
@@ -13,7 +13,7 @@ import helpers.compiler_functions as cf
 def main(file_path):
     program_counter: int = 0
 
-    line_count: int = cf.get_number_of_lines(file_path) + 1  # Added 1 so that we can include the HLT instruction at the end
+    line_count: int = cf.get_number_of_lines(file_path) 
     default_value1: PLine = PLine("")
     # Creation of line list, i.e. list that contains a PLine object for every line in the file
     pline_list: list[PLine] = [default_value1] * line_count
@@ -36,7 +36,7 @@ def main(file_path):
                 if pline_instance.text.startswith('   '):
                     cf.set_parent(pline_instance, pline_list, i)
                     next_line = next(file)
-                    if (next_line.strip().isspace() or not next_line.startswith('   ')) and (not pline_instance.parent.text.startswith("Else")): # if next line is blank or not indented and it has a parent, then it is the last line in the code block
+                    if (next_line.strip().isspace() or not next_line.startswith('   ')) and (not pline_instance.parent.text.startswith("else")): # if next line is blank or not indented and it has a parent, then it is the last line in the code block
                         program_counter += 3 # Always adds a jump of 3 bytes after the final child UNLESS it's at the end of an else statement
 
                 i += 1
@@ -44,7 +44,7 @@ def main(file_path):
                 # this line will execute the corresponding function based on the line type
                 program_counter += ce.switch_dict.get(pline_instance.type, ce.default_case)(pline_instance)
                 
-            pline_list[line_count] = cf.create_hlt("[End of Code]", program_counter, "hlt") # create hlt PLine Instance at end of the PLine list
+            pline_list.append(cf.create_hlt("[End of Code]", program_counter, "hlt")) # create hlt PLine Instance at end of the PLine list
             program_counter += 1                                                            # add 1 byte for HLT instruction
 
             # Add jump locations to if, else, and while sections of pline_list
